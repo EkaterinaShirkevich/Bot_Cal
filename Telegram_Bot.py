2f59e2ed2c66b73d9fe09098b0fb676cbbe12eff
Ekaterina_Shirkevich <eshirkevich@gmail.com>
2022-10-25 15:58:01 +0300

    del_bag_return&complex

--- Telegram_Bot.py
+++ Telegram_Bot.py
@@ -7,13 +7,13 @@ from telegram.ext import (
 )
 from telegram import ReplyKeyboardMarkup, ReplyKeyboardRemove, Update
 import logging
-from Info import Token
+#from Info import Token
 import excep as ex
 import logg
 import compl
 import json
 
-Token = Token()
+Token = '5645070358:AAHTJADMoQkedNwWP-48e11juNrBZYinHVI'
 
 
 logging.basicConfig(
@@ -47,18 +47,6 @@ def type_command(update, _):
                               '2.Комплексными')
     return ACTION
 
-# def return_type(update, _):
-#     # определяем пользователя
-#     user = update.message.from_user
-#     # Пишем в журнал сведения о фото
-#     logger.info("Пользователь %s хочет другой тип числа.", user.first_name)
-#     # Отвечаем на сообщение с пропущенной фотографией
-#     update.message.reply_text(
-#         'Ну, ладно. Можешь перевыбрать, или /cancel если хочешь завершить работу.'
-#     )
-#     # переходим к этапу `LOCATION`
-#     return TYPE
-
 
 def action_num(update, _):
     global action, type_num
@@ -74,7 +62,7 @@ def action_num(update, _):
                               'Умножение: "*"\n'
                               'Деление: "/"'
                               )
-    # action = update.message.text
+    #action = update.message.text
     return GIVE_NUM
 
 
@@ -83,44 +71,56 @@ def give_num(update, _):
     user = update.message.from_user
     logger.info("Действие %s: %s", user.first_name, update.message.text)
     action = update.message.text
-    if type_num == 1:
-        update.message.reply_text('Введите 2 числа через пробел: ')
-    elif type_num == 2:
-        update.message.reply_text('Введите 4 числа через пробел: ')
-
-    return RESULT
+    if action == '/return':
+        update.message.reply_text(
+        'Ну, ладно. Можешь перевыбрать, или /cancel если хочешь завершить работу.'
+        f'Выбери с какими числами хочешь работать?\n\n'
+                              '1.Рациональными \n'
+                              '2.Комплексными')
+        return ACTION
+    elif action != '/return':
+        if type_num == 1:
+            update.message.reply_text('Введите 2 числа через пробел: ')
+        elif type_num == 2:
+            update.message.reply_text('Введите 4 числа через пробел: ')
+        return RESULT
 
 
 def res(update, _):
+    reply_keyboard = [['Продолжить']]
+    markup_key = ReplyKeyboardMarkup(reply_keyboard, one_time_keyboard=True)
     global type_num, action, num1
     user = update.message.from_user
     logger.info("Действие %s: %s", user.first_name, update.message.text)
     if type_num == 1:
         num1 = update.message.text
         num1 = num1.replace(' ', action)
-        update.message.reply_text(f'{num1}={round(eval(num1))}')
+        update.message.reply_text(f'{num1}={round(eval(num1))}', reply_markup=markup_key,)
 
     elif type_num == 2:
         num1 = update.message.text
         k = compl.cal_compl(num1, action)
         print(k)
-        update.message.reply_text(f'{k}',
-                                  reply_markup=ReplyKeyboardRemove()
-                                  )
-
+        update.message.reply_text(f'{k}',reply_markup=markup_key,)
+                                  #reply_markup=ReplyKeyboardRemove())
     return MENU
 
 
 def menu(update, _):
-    # user = update.message.from_bot
-    # logger.info("Ответ бота: %s. Пользователь", user.first_name,"Ждет указаний")
+    user = update.message.from_user
+    logger.info("Ответ бота: %s. Пользователь", user.first_name,"Ждет указаний")
+    reply_keyboard = [['Начнем']]
+    markup_key = ReplyKeyboardMarkup(reply_keyboard, one_time_keyboard=True)
     update.message.reply_text(
         'Чтобы снова посчитать /start\n'
         'Хочешь завершить работу /cancel\n\n'
         'Твои действия?'
-
     )
     if update.message.from_user == '/start':
+        update.message.reply_text(f'Я могу посчитать еще \n'
+                              'Команда /cancel, чтобы прекратить разговор.\n\n'
+                              'Начнем?',
+                              reply_markup=markup_key,)
         return CommandHandler('start', type_command)
     else:
         return CommandHandler('cancel', cancel)
--- compl.py
+++ compl.py
@@ -14,23 +14,23 @@ def cal_compl(ls,i):
 	
 	if i == "+":
 		logg.result_logger(a + b)
-		return a + b
+		return complex(a + b)
 	elif i == "-":
 		logg.result_logger(a - b)
-		return a - b
+		return complex(a - b)
 	elif i == "*":
 		logg.result_logger(a * b)
-		return a * b
+		return complex(a * b)
 	elif i == "/" and b != 0:
 		logg.result_logger(a / b)
-		return a / b
+		return complex(a / b)
 	elif i == "/" and b == 0:
 		print("На ноль делить нельзя")
 		text= "Пользователь ввел: 0. Это некорректный ввод"
 		logg.actions_logger(text)
 	elif i == "^":
 		logg.result_logger(a ** b)
-		return a ** b
+		return complex(a ** b)
 
 # ls = '12 2 2 12'
 # print(cal_compl(ls,'+'))
