e54b3bf2ab27cc0f66b7576aac108bcc5ecb10bd
Ekaterina_Shirkevich <eshirkevich@gmail.com>
2022-10-27 18:15:23 +0300

    nums log & del excess data

--- compl.py
+++ compl.py
@@ -1,5 +1,3 @@
-from cgitb import text
-import excep as ex
 import logg
 
 def cal_compl(ls,i):
@@ -13,25 +11,18 @@ def cal_compl(ls,i):
 	b = complex(in_real_2, in_imag_2)
 	
 	if i == "+":
-		# logg.result_logger(a + b)
 		return complex(a + b)
 	elif i == "-":
-		# logg.result_logger(a - b)
 		return complex(a - b)
 	elif i == "*":
-		# logg.result_logger(a * b)
 		return complex(a * b)
 	elif i == "/" and b != 0:
-		# logg.result_logger(a / b)
 		return complex(a / b)
 	elif i == "/" and b == 0:
 		print("На ноль делить нельзя")
 		text= "Пользователь ввел: 0. Это некорректный ввод"
 		logg.actions_logger(text)
 	elif i == "^":
-		# logg.result_logger(a ** b)
 		return complex(a ** b)
 
-# ls = '12 2 2 12'
-# print(cal_compl(ls,'+'))
 
